b316c89c3c11c7be4549917a66f9beda1cdd7721
ekaterinalitvinenko <tim.ekaterina96@gmail.com>
2024-04-13 15:50:00 +0200

    requirements.txt

--- inspection_api/settings.py
+++ inspection_api/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = 'django-insecure-a)jczb2nzoap$u*2r#e3uky78m9t##s$hxokxv=93rf+3mtx88
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
 # Application definition
 
